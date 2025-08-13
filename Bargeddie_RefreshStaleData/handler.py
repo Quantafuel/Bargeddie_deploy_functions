cdb1228e5b72f99d5a22fd6946de33d0c652c719
henrikbreivik <96174495+henrikbreivik@users.noreply.github.com>
2025-08-13 15:18:17 +0200

    Refresh data (#6)
    * ex function1
    
    * ex function1
    
    * ex function1
    
    * ex function1
    
    * Recommit functions through oidc
    
    * Added new tags after asset hierarchy update
    
    * Added new tags after asset hierarchy update, both functions
    
    * RefreshData function commit
    
    * RefreshData update

--- Bargeddie_RefreshStaleData/handler.py
+++ Bargeddie_RefreshStaleData/handler.py
@@ -13,15 +13,17 @@ def handle(client):
         'BRGD:s="DB_2-03-1-GA201"."Momentanwert_t_h"',
     ]
 
-    seconds_ago = 58
-
     now = datetime.now()
-    td_ago = now - timedelta(seconds=seconds_ago)
+    last_min_stop = now.replace(second=0, microsecond=0)
+    last_min_start = last_min_stop - timedelta(seconds=60)
 
     for i in ext_ids:
         latest_dp = client.time_series.data.retrieve_latest(external_id=i)
         latest_dp_datetime = datetime.fromtimestamp(latest_dp.timestamp[0] / 1000)
 
-        if td_ago > latest_dp_datetime:
-            dp_insert = [(now, latest_dp.value[0])]
+        dp_insert = []
+
+        if last_min_start > latest_dp_datetime:
+            for k in range(0, 60):
+                dp_insert.append((last_min_start.replace(second=k), latest_dp.value[0]))
             client.time_series.data.insert(dp_insert, external_id=i)
